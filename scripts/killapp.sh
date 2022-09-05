c3fd56ec4dc775ed70a008c1bbc2350981b451ed
LuLu <727084494@qq.com>
2022-09-05 15:12:25 +0800

    shell script

--- scripts/killapp.sh
+++ scripts/killapp.sh
@@ -2,8 +2,8 @@ pid=$(ps -aux | grep dunwu | grep -v grep | awk '{print $2}')
 if [ "$pid" = '' ];then
   echo 'not found progress...'
 else
-   echo found progress, pid: ${i}, now killing it!
-   kill -9 $pid
+   echo found progress, pid: ${pid}, now killing it!
+   sudo kill -9 $pid
 fi
 pwd
 echo $(nohup java -Dtomcat.connector.port=9010 -cp ./target/JavaWebApp/WEB-INF/classes:./target/JavaWebApp/WEB-INF/lib/* io.github.dunwu.Main > temp.log &)
